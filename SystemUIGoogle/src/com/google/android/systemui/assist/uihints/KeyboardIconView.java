DirtyUnicorns/android_external_google

Fix views margin when navigation bar is hidden
Change-Id: I8a3d223669a1f7fff9ad645ec1442cbe3e6e1719

SystemUIGoogle/src/com/google/android/systemui/assist/uihints/KeyboardIconView.java
@@ -9,14 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import com.android.internal.util.du.Utils;
 import com.android.systemui.R;
 import com.google.android.systemui.assist.uihints.OverlayUiHost;
 
 public class KeyboardIconView extends FrameLayout implements OverlayUiHost.BottomMarginListener {
     private final int COLOR_DARK_BACKGROUND;
     private final int COLOR_LIGHT_BACKGROUND;
-    private ImageView mKeyboardIcon;
+    private final int MINIMUM_MARGIN_PX;
     private final int mMargin;
+    private ImageView mKeyboardIcon;
+    private boolean mShowAssistUi;
+    private Context mContext;
 
     public KeyboardIconView(Context context) {
         this(context, null);
@@ -32,14 +36,15 @@ public class KeyboardIconView extends FrameLayout implements OverlayUiHost.Botto
 
     public KeyboardIconView(Context context, AttributeSet attributeSet, int i, int i2) {
         super(context, attributeSet, i, i2);
+        mContext = context;
         COLOR_DARK_BACKGROUND = getResources().getColor(R.color.transcription_icon_dark);
         COLOR_LIGHT_BACKGROUND = getResources().getColor(R.color.transcription_icon_light);
+        MINIMUM_MARGIN_PX = DisplayUtils.convertDpToPx(16.0f, mContext);
         mMargin = getResources().getDimensionPixelSize(R.dimen.keyboard_icon_bottom_margin);
     }
 
-    public void onBottomMarginChanged(int i) {
-        ((ViewGroup.MarginLayoutParams) getLayoutParams()).bottomMargin = i + mMargin;
-        requestLayout();
+    public void onBottomMarginChanged() {
+        setIconBottomMargin();
     }
 
     @Override
@@ -52,13 +57,17 @@ public class KeyboardIconView extends FrameLayout implements OverlayUiHost.Botto
         mKeyboardIcon.setImageTintList(ColorStateList.valueOf(z ? COLOR_DARK_BACKGROUND : COLOR_LIGHT_BACKGROUND));
     }
 
-    public void show(PendingIntent pendingIntent) {
+    public void show(PendingIntent pendingIntent, boolean state) {
         if (pendingIntent != null) {
+            mShowAssistUi = state;
+            setIconBottomMargin();
             mKeyboardIcon.setOnClickListener(view -> lambda$show$00(pendingIntent, view));
             setVisibility(0);
             return;
         }
+        mKeyboardIcon.setImageDrawable(null);
         mKeyboardIcon.setOnClickListener(null);
+        mShowAssistUi = false;
         setVisibility(8);
         Log.w("KeyboardIconView", "No keyboard intent specified");
     }
@@ -72,10 +81,26 @@ public class KeyboardIconView extends FrameLayout implements OverlayUiHost.Botto
     }
 
     public void hide() {
+        mKeyboardIcon.setImageDrawable(null);
+        mShowAssistUi = false;
         setVisibility(8);
     }
 
-    void onDensityChanged() {
+    public void setIconBottomMargin() {
         mKeyboardIcon.setImageDrawable(getContext().getDrawable(R.drawable.ic_keyboard));
+        ((ViewGroup.MarginLayoutParams) getLayoutParams()).bottomMargin = getBottomMargin() + mMargin;
+        requestLayout();
+    }
+
+    private int getBottomMargin() {
+        if (mShowAssistUi) {
+            return MINIMUM_MARGIN_PX;
+        } else {
+            return Utils.shouldSetNavbarHeight(mContext) ? MINIMUM_MARGIN_PX : 0;
+        }
+    }
+
+    void onDensityChanged() {
+        setIconBottomMargin();
     }
 }

SystemUIGoogle/src/com/google/android/systemui/assist/uihints/ZeroStateIconView.java
@@ -9,14 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import com.android.internal.util.du.Utils;
 import com.android.systemui.R;
 import com.google.android.systemui.assist.uihints.OverlayUiHost;
 
 public class ZeroStateIconView extends FrameLayout implements OverlayUiHost.BottomMarginListener {
     private final int COLOR_DARK_BACKGROUND;
     private final int COLOR_LIGHT_BACKGROUND;
+    private final int MINIMUM_MARGIN_PX;
     private final int mMargin;
     private ImageView mZeroStateIcon;
+    private boolean mShowAssistUi;
+    private Context mContext;
 
     public ZeroStateIconView(Context context) {
         this(context, null);
@@ -32,14 +36,15 @@ public class ZeroStateIconView extends FrameLayout implements OverlayUiHost.Bott
 
     public ZeroStateIconView(Context context, AttributeSet attributeSet, int i, int i2) {
         super(context, attributeSet, i, i2);
+        mContext = context;
         COLOR_DARK_BACKGROUND = getResources().getColor(R.color.transcription_icon_dark);
         COLOR_LIGHT_BACKGROUND = getResources().getColor(R.color.transcription_icon_light);
+        MINIMUM_MARGIN_PX = DisplayUtils.convertDpToPx(16.0f, mContext);
         mMargin = getResources().getDimensionPixelSize(R.dimen.zerostate_icon_bottom_margin);
     }
 
-    public void onBottomMarginChanged(int i) {
-        ((ViewGroup.MarginLayoutParams) getLayoutParams()).bottomMargin = i + mMargin;
-        requestLayout();
+    public void onBottomMarginChanged() {
+        setIconBottomMargin();
     }
 
     @Override
@@ -52,13 +57,17 @@ public class ZeroStateIconView extends FrameLayout implements OverlayUiHost.Bott
         mZeroStateIcon.setImageTintList(ColorStateList.valueOf(z ? COLOR_DARK_BACKGROUND : COLOR_LIGHT_BACKGROUND));
     }
 
-    public void show(PendingIntent pendingIntent) {
+    public void show(PendingIntent pendingIntent, boolean state) {
         if (pendingIntent != null) {
+            mShowAssistUi = state;
+            setIconBottomMargin();
             mZeroStateIcon.setOnClickListener(view -> lambda$show$00(pendingIntent, view));
             setVisibility(0);
             return;
         }
+        mZeroStateIcon.setImageDrawable(null);
         mZeroStateIcon.setOnClickListener(null);
+        mShowAssistUi = false;
         setVisibility(8);
         Log.w("ZeroStateIconView", "No zerostate intent specified");
     }
@@ -72,10 +81,26 @@ public class ZeroStateIconView extends FrameLayout implements OverlayUiHost.Bott
     }
 
     public void hide() {
+        mZeroStateIcon.setImageDrawable(null);
+        mShowAssistUi = false;
         setVisibility(8);
     }
 
-    void onDensityChanged() {
+    public void setIconBottomMargin() {
         mZeroStateIcon.setImageDrawable(getContext().getDrawable(R.drawable.ic_explore));
+        ((ViewGroup.MarginLayoutParams) getLayoutParams()).bottomMargin = getBottomMargin() + mMargin;
+        requestLayout();
+    }
+
+    private int getBottomMargin() {
+        if (mShowAssistUi) {
+            return MINIMUM_MARGIN_PX;
+        } else {
+            return Utils.shouldSetNavbarHeight(mContext) ? MINIMUM_MARGIN_PX : 0;
+        }
+    }
+
+    void onDensityChanged() {
+        setIconBottomMargin();
     }
 }

AOSIP/platform_external_google

SystemUIGoogle/src/com/google/android/systemui/assist/uihints/KeyboardIconView.java
@@ -9,14 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import com.android.internal.util.aosip.aosipUtils;
 import com.android.systemui.R;
 import com.google.android.systemui.assist.uihints.OverlayUiHost;
 
 public class KeyboardIconView extends FrameLayout implements OverlayUiHost.BottomMarginListener {
     private final int COLOR_DARK_BACKGROUND;
     private final int COLOR_LIGHT_BACKGROUND;
-    private ImageView mKeyboardIcon;
+    private final int MINIMUM_MARGIN_PX;
     private final int mMargin;
+    private ImageView mKeyboardIcon;
+    private boolean mShowAssistUi;
+    private Context mContext;
 
     public KeyboardIconView(Context context) {
         this(context, null);
@@ -32,14 +36,15 @@ public class KeyboardIconView extends FrameLayout implements OverlayUiHost.Botto
 
     public KeyboardIconView(Context context, AttributeSet attributeSet, int i, int i2) {
         super(context, attributeSet, i, i2);
+        mContext = context;
         COLOR_DARK_BACKGROUND = getResources().getColor(R.color.transcription_icon_dark);
         COLOR_LIGHT_BACKGROUND = getResources().getColor(R.color.transcription_icon_light);
+        MINIMUM_MARGIN_PX = DisplayUtils.convertDpToPx(16.0f, mContext);
         mMargin = getResources().getDimensionPixelSize(R.dimen.keyboard_icon_bottom_margin);
     }
 
-    public void onBottomMarginChanged(int i) {
-        ((ViewGroup.MarginLayoutParams) getLayoutParams()).bottomMargin = i + mMargin;
-        requestLayout();
+    public void onBottomMarginChanged() {
+        setIconBottomMargin();
     }
 
     @Override
@@ -52,13 +57,17 @@ public class KeyboardIconView extends FrameLayout implements OverlayUiHost.Botto
         mKeyboardIcon.setImageTintList(ColorStateList.valueOf(z ? COLOR_DARK_BACKGROUND : COLOR_LIGHT_BACKGROUND));
     }
 
-    public void show(PendingIntent pendingIntent) {
+    public void show(PendingIntent pendingIntent, boolean state) {
         if (pendingIntent != null) {
+            mShowAssistUi = state;
+            setIconBottomMargin();
             mKeyboardIcon.setOnClickListener(view -> lambda$show$00(pendingIntent, view));
             setVisibility(0);
             return;
         }
+        mKeyboardIcon.setImageDrawable(null);
         mKeyboardIcon.setOnClickListener(null);
+        mShowAssistUi = false;
         setVisibility(8);
         Log.w("KeyboardIconView", "No keyboard intent specified");
     }
@@ -72,10 +81,26 @@ public class KeyboardIconView extends FrameLayout implements OverlayUiHost.Botto
     }
 
     public void hide() {
+        mKeyboardIcon.setImageDrawable(null);
+        mShowAssistUi = false;
         setVisibility(8);
     }
 
-    void onDensityChanged() {
+    public void setIconBottomMargin() {
         mKeyboardIcon.setImageDrawable(getContext().getDrawable(R.drawable.ic_keyboard));
+        ((ViewGroup.MarginLayoutParams) getLayoutParams()).bottomMargin = getBottomMargin() + mMargin;
+        requestLayout();
+    }
+
+    private int getBottomMargin() {
+        if (mShowAssistUi) {
+            return MINIMUM_MARGIN_PX;
+        } else {
+            return aosipUtils.shouldSetNavbarHeight(mContext) ? MINIMUM_MARGIN_PX : 0;
+        }
+    }
+
+    void onDensityChanged() {
+        setIconBottomMargin();
     }
 }

SystemUIGoogle/src/com/google/android/systemui/assist/uihints/ZeroStateIconView.java
@@ -9,14 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import com.android.internal.util.aosip.aosipUtils;
 import com.android.systemui.R;
 import com.google.android.systemui.assist.uihints.OverlayUiHost;
 
 public class ZeroStateIconView extends FrameLayout implements OverlayUiHost.BottomMarginListener {
     private final int COLOR_DARK_BACKGROUND;
     private final int COLOR_LIGHT_BACKGROUND;
+    private final int MINIMUM_MARGIN_PX;
     private final int mMargin;
     private ImageView mZeroStateIcon;
+    private boolean mShowAssistUi;
+    private Context mContext;
 
     public ZeroStateIconView(Context context) {
         this(context, null);
@@ -32,14 +36,15 @@ public class ZeroStateIconView extends FrameLayout implements OverlayUiHost.Bott
 
     public ZeroStateIconView(Context context, AttributeSet attributeSet, int i, int i2) {
         super(context, attributeSet, i, i2);
+        mContext = context;
         COLOR_DARK_BACKGROUND = getResources().getColor(R.color.transcription_icon_dark);
         COLOR_LIGHT_BACKGROUND = getResources().getColor(R.color.transcription_icon_light);
+        MINIMUM_MARGIN_PX = DisplayUtils.convertDpToPx(16.0f, mContext);
         mMargin = getResources().getDimensionPixelSize(R.dimen.zerostate_icon_bottom_margin);
     }
 
-    public void onBottomMarginChanged(int i) {
-        ((ViewGroup.MarginLayoutParams) getLayoutParams()).bottomMargin = i + mMargin;
-        requestLayout();
+    public void onBottomMarginChanged() {
+        setIconBottomMargin();
     }
 
     @Override
@@ -52,13 +57,17 @@ public class ZeroStateIconView extends FrameLayout implements OverlayUiHost.Bott
         mZeroStateIcon.setImageTintList(ColorStateList.valueOf(z ? COLOR_DARK_BACKGROUND : COLOR_LIGHT_BACKGROUND));
     }
 
-    public void show(PendingIntent pendingIntent) {
+    public void show(PendingIntent pendingIntent, boolean state) {
         if (pendingIntent != null) {
+            mShowAssistUi = state;
+            setIconBottomMargin();
             mZeroStateIcon.setOnClickListener(view -> lambda$show$00(pendingIntent, view));
             setVisibility(0);
             return;
         }
+        mZeroStateIcon.setImageDrawable(null);
         mZeroStateIcon.setOnClickListener(null);
+        mShowAssistUi = false;
         setVisibility(8);
         Log.w("ZeroStateIconView", "No zerostate intent specified");
     }
@@ -72,10 +81,26 @@ public class ZeroStateIconView extends FrameLayout implements OverlayUiHost.Bott
     }
 
     public void hide() {
+        mZeroStateIcon.setImageDrawable(null);
+        mShowAssistUi = false;
         setVisibility(8);
     }
 
-    void onDensityChanged() {
+    public void setIconBottomMargin() {
         mZeroStateIcon.setImageDrawable(getContext().getDrawable(R.drawable.ic_explore));
+        ((ViewGroup.MarginLayoutParams) getLayoutParams()).bottomMargin = getBottomMargin() + mMargin;
+        requestLayout();
+    }
+
+    private int getBottomMargin() {
+        if (mShowAssistUi) {
+            return MINIMUM_MARGIN_PX;
+        } else {
+            return Utils.shouldSetNavbarHeight(mContext) ? MINIMUM_MARGIN_PX : 0;
+        }
+    }
+
+    void onDensityChanged() {
+        setIconBottomMargin();
     }
 }